0abffe1c26a9c46a062a0832a92f13bf040f09dd
HiKat <pekumrp@gmail.com>
2015-06-29 18:59:59 +0900

    mysyntax.rktを変更.

--- analy-fucn_proto_st.rkt
+++ analy-fucn_proto_st.rkt
@@ -9,6 +9,15 @@
 (define current-lev 0)
 (define env '())
 
+;プロトタイプ宣言を行った関数の
+;返り値がnormalかpointerか、パラメータの有無がnoremalかnoneか
+;を保存するフラグ
+(struct para_flag (out-type para))
+
+
+
+
+;analy-func_proto_stは
 ;(stx:func_proto_st...)
 ;を受け取って
 ;(stx:func_proto_st (stx:spec_st...) 
@@ -21,17 +30,43 @@
 ;パラメータ専用の環境をまず初期化してから登録
 ;これは初期化->エラーチェックもしくはエラーチェック->初期化 ?????????????????????
 (define (analy-func_proto_st st)
-  (let* (;このfunc-typeがintで返り値が*intの場合あり.
+  ;;;;;内部定義
+  ;make-obj-from-paralistは
+  ;(list* (stx:para_declaration_st...)...)
+  ;を引数に取り、
+  ;(list obj...)
+  ;を返す.
+  ;'noparaの時は関数の外で処理する.
+  (define　(make-obj-from-paralist para-list)
+    (map* (lambda (para-decl) 
+            (let* (;typeは'intとか.この時点では確定しない.最終的にはflagと合わせて決定.
+                   (type (stx:spec_st-type (stx:para_declaration_st-type-spec para-decl)))
+                   ;idはstx:id_stかstx:id_ast_st
+                   (id (stx:para_declaration_st-para para-decl))
+                    ;flagはポインタ型なら'pointer、そうでなければ'normal
+                   ;flagとtypeは1つにまとめることも可能
+                   (flag (cond ((stx:id_st? id) 'normal)
+                               ((stx:id_ast_st? id) 'pointer)))
+                   (name (cond ((eq? flag 'normal) (stx:id_st-name id))
+                               ((eq? flag 'pointer)(stx:id_ast_st-name id))))
+                   (lev 1)
+                   (kind 'parm)            
+                   (type (cond ((eq? flag 'normal) type)
+                               ((eq? flag 'pointer)(list 'pointer type)))))
+              (obj name lev kind type)))
+          para-list))
+   ;;;;;内部定義ここまで                
+  (let* (;このspecがintで返り値が*intの場合あり.
          ;返り値は最終的にはここの型とflagで決定される.
-         ;func-typeはstx:spec_st
-         (func-type (stx:func_proto_st-type-spec st))
+         ;specはstx:spec_st
+         (spec (stx:func_proto_st-type-spec st))
          ;declはstx:func_declarator/_null/_ast/_stの4つの場合がある.
          (decl (stx:func_proto_st-func-declarator st))
          ;返り値がnormalかpointerか、パラメータの有無がnoremalかnoneか
-         (flag (cond ((stx:func_declarator_st? decl)(stx:func_declarator_ast_null_st? decl)'normal)
-                     ((stx:func_declarator_null_st? decl)(list 'normal 'none))
-                     ((stx:func_declarator_ast_st? decl) (list 'pointer 'normal))
-                     ((stx:func_declarator_ast_null_st? decl) (list 'pointer 'none))))
+         (flag (cond ((stx:func_declarator_st? decl)(para_flag 'normal 'normal))
+                     ((stx:func_declarator_null_st? decl)(para_flag 'normal 'none))
+                     ((stx:func_declarator_ast_st? decl) (para_flag 'pointer 'normal))
+                     ((stx:func_declarator_ast_null_st? decl) (para_flag 'pointer 'none))))
          (proto-name (cond ((stx:func_declarator_st? decl) 
                             (stx:func_declarator_st-name decl))
                            ((stx:func_declarator_null_st? decl)
@@ -40,11 +75,33 @@
                             (stx:func_declarator_ast_st-name decl))
                            ((stx:func_declarator_ast_null_st? decl) 
                             (stx:func_declarator_ast_null_st-name decl))))
-         (proto-type "under const")
-         (proto-obj (obj proto-name 0 'proto proto-type)))
-    (check-env proto-obj env)
-    (set! env (extend proto-obj env))
+         ;para-listは(list* (stx:para_declaration_st...)...)
+         ;もしくはパラメータが無いときは'noparaが入っている.
+         (para-list (cond ((stx:func_declarator_st? decl) 
+                            (stx:func_declarator_st-para-list decl))
+                           ((stx:func_declarator_null_st? decl)
+                            'nopara)
+                           ((stx:func_declarator_ast_st? decl) 
+                            (stx:func_declarator_ast_st-para-list decl))
+                           ((stx:func_declarator_ast_null_st? decl) 
+                            'nopara)))
+         (para-obj-list (cond ((eq? para-list 'nopara) 'nopara)
+                              (else (make-obj-from-paralist paralist))))
+    
          
+         (proto-type "specの値とflagで決まる.")
+         (proto-obj (obj proto-name 0 'proto proto-type)))
+
+    (set! env (extend proto-obj env))
+    ;返す構造体
+    ;ここで返したいものはlet*で取り出しておく必要がある.
+    (stx:func_prot_st spec  
+                      (cond ;para-obj-listはパラメータのobjのlist
+                        ((eq? 'noramal (para_flag-out-type flag)) 
+                         (stx:func_declarator_st proto-name para-obj-list))
+                        ((eq? 'pointer (para_flag-out-type flag)) 
+                         (stx:func_declarator_ast_st proto-name para-obj-list))))))
+                      
 
 
 (define test1

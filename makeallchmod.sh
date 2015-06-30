ae3434720f515ec83fe073cab44b810d8fb39c28
HiKat <pekumrp@gmail.com>
2015-06-30 11:05:15 +0900

    テスト

--- makeallchmod.sh
+++ makeallchmod.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
-#以下のディレクトリ内の全てのシェルスクリプトにchmodを適用する。
-#シェルスクリプトのディレクトリ
-SHDIR=~/workspace/github/compiler/*.sh
+#以下で指定した全てのシェルスクリプトにchmodを適用する。
+SHPATH=~/workspace/github/compiler/*.sh
 #コマンド
-chmod 755 $SHDIR
+chmod 755 $SHPATH
 
